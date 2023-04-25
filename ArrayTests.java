ce04415122d08c34b7c20ec11261667174e60f27
Kriish Mehta <62770042+itzkriish@users.noreply.github.com>
2023-04-24 21:50:59 -0700

    Fixed bugs in ArrayTests reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,20 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] input1 = { 0, 1 };
+    ArrayExamples.reverseInPlace(input1);
+    // error as it produces {1, 1} since it uses el at ind 0 which it has already changed
+    assertArrayEquals(new int[]{ 1, 0 }, input1);
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = { 1 };
+    // error as it tries to assign a value from the new array which is empty to the old array
+    // also returns original array
+    assertArrayEquals(new int[]{ 1 }, ArrayExamples.reversed(input1));
+  }
 }
